589bc4587fcaddb48d55bd2c9748fba23cb9a805
Kaliberda Anton <73352390+JestAK@users.noreply.github.com>
2025-02-25 01:25:54 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"net/http"
-	"time"
+  "encoding/json"
+  "log"
+  "net/http"
+  "time"
 )
 
-func timeManager(responseWriter http.ResponseWriter, request *http.Request) {
-	if request.Method != http.MethodGet {
-		http.Error(responseWriter, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
+func timeHandler(responseWriter http.ResponseWriter, request *http.Request) {
+  if request.Method != http.MethodGet {
+    http.Error(responseWriter, "Method not allowed", http.StatusMethodNotAllowed)
+    return
+  }
 
-	currTime := map[string]string{"time": time.Now().Format(time.RFC3339)}
-	responseWriter.Header().Set("Content-Type", "application/json")
+  currentTime := map[string]string{"time": time.Now().Format(time.RFC3339)}
+  responseWriter.Header().Set("Content-Type", "application/json")
 
-	if err := json.NewEncoder(responseWriter).Encode(currTime); err != nil {
-		http.Error(responseWriter, "Failed to encode response", http.StatusInternalServerError)
-		log.Println("Error encoding response:", err)
-	}
+  if err := json.NewEncoder(responseWriter).Encode(currentTime); err != nil {
+    http.Error(responseWriter, "Failed to encode response", http.StatusInternalServerError)
+    log.Println("Error encoding response:", err)
+  }
 }
 
 func main() {
-	http.HandleFunc("/time", timeManager)
-	PORT := ":8795"
-	log.Println("Server is running on http://localhost" + PORT)
+  http.HandleFunc("/time", timeHandler)
+  port := ":8795"
+  log.Println("Server is running on http://localhost" + port)
 
-	if err := http.ListenAndServe(PORT, nil); err != nil {
-		log.Fatal("Server failed:", err)
-	}
+  if err := http.ListenAndServe(port, nil); err != nil {
+    log.Fatal("Server failed:", err)
+  }
 }
